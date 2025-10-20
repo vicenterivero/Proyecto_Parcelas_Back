6b3ab39696cd3364b6a6b537fc8d18d650fd8ce5
Vicente Rivero <vrivero@dexabyte.com.mx>
2025-10-20 18:24:09 -0500

    all cors

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 
 // Configurar CORS
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
